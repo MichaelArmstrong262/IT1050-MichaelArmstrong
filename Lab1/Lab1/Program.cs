e5361f013a35e1f3ddea915a263aab7cf88acb5a
Michael Armstrong <S01066263@acad.tri-c.edu>
2018-02-03 16:15:35 -0500

    First lab for IT1010.

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Hello Wrold
+// Text-Displaying app.
+using System;
+
+class Hello1
+{
+    // Main method begins execution of C# app
+    static void Main()
+    {
+        string s1 = "Hello world from ";
+        string s2 = "Michael Armstrong!";
+
+        s1 += s2;
+        System.Console.WriteLine(s1);
+        Console.WriteLine("Did I do the extra credit assignment correctly?");
+        Console.WriteLine("Press Any Key to exit this application");
+        Console.ReadKey();
+    } // end Main
+} //end class Hello1
